63e37752564bd55d9b1a623de48261807217b272
Vladislav Rastoropov <rastmath23@gmail.com>
2022-03-13 18:19:49 +0300

    added preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,22 +1,30 @@
-import Footer from "../Footer/Footer";
+import Preloader from "../Preloader/Preloader";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import SearchForm from "../SearchForm/SearchForm";
+import { useState } from "react";
 
 
 const Movies = () => {
+  const [ isLoaded, setIsLoaded ] = useState(true);
+  const [ isFound, setIsFound ] = useState(true);
+
   return (
     <>
       <main className="movies">
-        <SearchForm
-
-        />
+        <SearchForm />
         {/* <section className="movies-result"> */}
-          <MoviesCardList />
+
+        { isLoaded ?
+          (isFound ? <MoviesCardList /> : <p className="movies__not-found">Ничего не найдено</p>)
+          :
+          <Preloader />
+        }
+        {/* <MoviesCardList /> */}
           {/* <button type="button" className="movies-result__more-movies app__link">Ещё</button> */}
         {/* </section> */}
 
       </main>
-      
+
     </>
   );
 };
